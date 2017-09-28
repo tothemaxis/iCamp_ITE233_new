4844720db70dfc70a1568060c0f33fc1ab36ce74
ToTheMax <sethapakdi@gmail.com>
2017-09-28 13:45:36 +0700

    first commit

---
+++ iCamp_ITE233_new/register_form.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iCamp_ite233
+{
+    public partial class register_form : Form
+    {
+        public register_form()
+        {
+            InitializeComponent();
+        }
+
+        private void register_form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
